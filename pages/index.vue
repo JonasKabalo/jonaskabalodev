97d19653d426099e48b352dce0a35e8c7e4d2596
jonaskabalo <kabalojonas44@gmail.com>
2021-07-06 14:27:47 +0100

    test script on nuxt wth vue lifehook

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,25 @@
   <div>
     <h1 class="text-center">
       Home
+      {{ dd }}
     </h1>
   </div>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    dd: 'test'
+  }),
+  mounted () {
+    console.log('here')
+    this.test()
+  },
+  methods: {
+    test () {
+      console.log('hreeeeee')
+    }
+  }
 }
 </script>
 
